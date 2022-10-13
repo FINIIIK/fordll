8c29fdef682a4484249ea091f191519fd8e8abd7
FINIIIK <94627737+FINIIIK@users.noreply.github.com>
2022-10-14 02:14:27 +0500

    Add files via upload

---
+++ app/app/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using dllLIB;
+
+namespace dllLIB
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double x, y;
+            Console.Write("Введите первое число: ");
+            x = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите второе число: ");
+            y = Convert.ToDouble(Console.ReadLine());
+            Class1.PlusFunc(x, y);
+            //Class1.MinusFunc(x, y);
+            //Class1.UmnojitFunc(i, j);
+            //Class1.RazdelitFunc(i, j);
+            //Class1.StepenFunc(i, j);
+        }
+    }
+}
